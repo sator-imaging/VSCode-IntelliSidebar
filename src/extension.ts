8761f8466b99ccbffe54f14c44445b55c6ffc802
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-01-25 11:00:22 +0900

    fix: don't steal focus unexpectedly

--- src/extension.ts
+++ src/extension.ts
@@ -5,25 +5,37 @@ import * as vscode from 'vscode';
 // export function deactivate() {
 // }
 
+let lastTabGroupCount = -1;
 
 export function activate(context: vscode.ExtensionContext) {
 
 	let onActiveEditorChanged = vscode.window.onDidChangeActiveTextEditor(editor => {
+		const currentTabGroupCount = vscode.window.tabGroups.all.length;
+		if (currentTabGroupCount === lastTabGroupCount) {
+			return;
+		}
+		lastTabGroupCount = currentTabGroupCount;
+
 		//let langId = editor?.document.languageId;
 		//vscode.window.showInformationMessage("LANG: " + langId);
-		if (vscode.window.tabGroups.all.length > 1) {
+		if (currentTabGroupCount > 1) {
 			vscode.commands.executeCommand('workbench.action.closeSidebar');
 
 			// // don't handle auxiliary bar
 			// vscode.commands.executeCommand('workbench.action.closeAuxiliaryBar');
 		}
-
 	});
 
 
 	let onTabGroupsChanged = vscode.window.tabGroups.onDidChangeTabGroups(tabGroups => {
+		const currentTabGroupCount = vscode.window.tabGroups.all.length;
+		if (currentTabGroupCount === lastTabGroupCount) {
+			return;
+		}
+		lastTabGroupCount = currentTabGroupCount;
+
 		//vscode.window.showInformationMessage('TAB GROUPS: ' + vscode.window.tabGroups.all.length);
-		if (vscode.window.tabGroups.all.length > 1) {
+		if (currentTabGroupCount > 1) {
 			vscode.commands.executeCommand('workbench.action.closeSidebar');
 
 			// // don't handle auxiliary bar
